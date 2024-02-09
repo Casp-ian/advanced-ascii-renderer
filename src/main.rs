46eb3d919b4fe9b1909a46891b352dd4e99af85e
Caspian <caspian@hp-pavilion-gaming-laptop-16-a06xxx.home>
2024-02-10 00:29:50 +0100

    added colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, ValueEnum};
-use image::{io::Reader, DynamicImage, GenericImageView, Rgba};
+use image::{io::Reader, DynamicImage, GenericImageView, Pixel, Rgba};
 
 
 // TODO add aspect ratio
@@ -20,47 +20,62 @@ struct Args {
 
     /// Color of text
     #[arg(long, default_value_t, value_enum)]
-    color: Color,
+    color: ColorSet,
 
     /// Characters used for result
     #[arg(long, default_value_t, value_enum)]
     set: CharSet,
 }
 
+const BLACK: &str = "\x1b[30m";
+const DARK_RED: &str = "\x1b[31m";
+const DARK_GREEN: &str = "\x1b[32m";
+const DARK_YELLOW: &str = "\x1b[33m";
+const DARK_BLUE: &str = "\x1b[34m";
+const DARK_MAGENTA: &str = "\x1b[35m";
+const DARK_CYAN: &str = "\x1b[36m";
+const LIGHT_GRAY: &str = "\x1b[37m";
+const DARK_GRAY: &str = "\x1b[90m";
+const RED: &str = "\x1b[91m";
+const GREEN: &str = "\x1b[92m";
+const ORANGE: &str = "\x1b[93m";
+const BLUE: &str = "\x1b[94m";
+const MANGENTA: &str = "\x1b[95m";
+const CYAN: &str = "\x1b[96m";
+const WHITE: &str = "\x1b[97m";
 
 #[derive(ValueEnum, Clone, Debug, Default, PartialEq)]
-enum Color {
+enum ColorSet {
     #[default]
     None,
     GreenBlue,
     BluePurple,
 }
 
-impl Color {
+impl ColorSet {
     fn get_color_prefix(&self, pixel: Rgba<u8>) -> &str {
-        if self == &Color::None {
+        if self == &ColorSet::None {
             return "";
         }
 
-        // TODO redo colors
         let red = pixel.0[0];
         let green = pixel.0[1];
         let blue = pixel.0[2];
 
         // TODO do these color codes only work on linux terminal?
-        if self == &Color::GreenBlue {
+        if self == &ColorSet::GreenBlue {
             if green > blue {
-                return "\x1b[32m";
+                return GREEN;
             } else {
-                return "\x1b[36m";
+                return BLUE;
             }
         }
 
-        if self == &Color::BluePurple {
+        if self == &ColorSet::BluePurple {
             if green > blue {
-                return "\x1b[92m";
+                return BLUE;
             } else {
-                return "\x1b[94m";
+                return DARK_MAGENTA;
             }
         }
 
@@ -111,7 +126,7 @@ impl CharSet {
 }
 
 fn get_brightness(pixel: Rgba<u8>) -> u8 {
-    // TODO to luma?
+    // TODO to luma? does it matter?
     let red = pixel.0[0];
     let green = pixel.0[1];
     let blue = pixel.0[2];
@@ -138,22 +153,20 @@ fn main() {
 }
 
 
-fn process_image(image: DynamicImage, width: u32, height: u32, set: CharSet, color: Color) -> String {
+fn process_image(image: DynamicImage, width: u32, height: u32, set: CharSet, color: ColorSet) -> String {
 
     let part_height = image.height() / height;
     let part_width = image.width() / width;
-
-    // TODO can optimize, most of the blurred pixels are never used
-    // let blurred = image.blur(2.);
-    let blurred = image.clone();
 
     let mut result = "".to_string();
 
     // iterate over parts of image
     for y in 0..height {
         for x in 0..width {
+
             // get average value of part
-            let pixel = blurred.get_pixel((part_width * x) + (part_width / 2), (part_height * y) + (part_height / 2));
+            let pixel = get_blended_pixel(&image, part_width * x, part_height * y, part_width, part_height);
+
             // place char in result string
             result += color.get_color_prefix(pixel);
             result += set.get_char(pixel);
@@ -162,4 +175,21 @@ fn process_image(image: DynamicImage, width: u32, height: u32, set: CharSet, col
     }
 
     return result;
+}
+
+fn get_blended_pixel(image: &DynamicImage, x: u32, y: u32, width: u32, height: u32) -> Rgba<u8> {
+    let _ = (1..6).map(f64::from);
+
+    // TODO clean up, for now it takes the average of 4 pixels spread around the given area
+    let mut pixel1 = image.get_pixel(x + (    width / 4),  y + (    height / 4));
+    let mut pixel2 = image.get_pixel(x + (    width / 4),  y + (3 * height / 4));
+    let mut pixel3 = image.get_pixel(x + (3 * width / 4),  y + (    height / 4));
+    let mut pixel4 = image.get_pixel(x + (3 * width / 4),  y + (3 * height / 4));
+
+    pixel1.blend(&pixel2);
+    pixel3.blend(&pixel4);
+
+    pixel1.blend(&pixel3);
+
+    return pixel1;
 }
