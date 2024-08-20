285d61d97e89b1067848b54ebd09fc84fd9f7d1f
caspian <caspian@koolwaaij.nl>
2024-08-20 23:43:47 +0200

    fix up ugly code, split out a processing step to prepare for the next big change and improve automatic resolution

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::u32;
 
 use clap::{Parser, ValueEnum};
-use image::{io::Reader, DynamicImage, GenericImageView, Pixel, Rgba};
+use image::{io::Reader, DynamicImage, GenericImageView, Rgba};
 
-
-// TODO add aspect ratio
-/// Simple program to greet a person
+/// Take an image and turn it into text
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Args {
@@ -13,8 +11,12 @@ struct Args {
     path: std::path::PathBuf,
 
     /// Width in characters
-    #[arg(long, default_value_t = 64)]
-    width: u32,
+    #[arg(long)]
+    width: Option<u32>,
+
+    /// Height in characters
+    #[arg(long)]
+    height: Option<u32>,
 
     /// Color of text
     #[arg(long, default_value_t, value_enum)]
@@ -24,9 +26,17 @@ struct Args {
     #[arg(long, default_value_t, value_enum)]
     set: CharSet,
 
-    /// Characters used for result
+    /// make dark areas light, and light areas dark
     #[arg(long)]
     inverted: bool,
+
+    /// remove the lines with /-\|
+    #[arg(long)]
+    no_lines: bool,
+
+    /// the width and height of a character in pixels, only use if the defaults dont suit your needs
+    #[arg(long, num_args(2))]
+    character_size: Option<Vec<u32>>,
 }
 
 const BLACK: &str = "\x1b[30m";
@@ -88,143 +98,228 @@ impl ColorSet {
 #[derive(ValueEnum, Clone, Debug, Default, PartialEq)]
 enum CharSet {
     #[default]
-    Filled,
     Braile,
+    Filled,
+    Numbers,
     Discord,
 }
 
 impl CharSet {
-    fn get_char(&self, pixel: Rgba<u8>) -> &str {
-        let brightness = get_brightness(pixel);
+    fn get_char(&self, brightness: f32) -> &str {
+        let set = match self {
+            &CharSet::Filled => vec![" ", ".", "\"", "+", "o", "?", "#"],
+            &CharSet::Numbers => vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+            &CharSet::Discord => vec![
+                ":black_large_square:",
+                ":new_moon:",
+                ":elephant:",
+                ":fog:",
+                ":white_large_square:",
+            ],
+            &CharSet::Braile => vec!["⠀", "⢀", "⡈", "⡊", "⢕", "⢝", "⣫", "⣟", "⣿"],
+        };
+        let id: f32 = brightness * (set.len() - 1) as f32;
 
-        if self == &CharSet::Filled {
-
-            // █#eo+,.     <- dont forget that one space is also a part
-
-            if brightness > 215 {
-                return "█";
-            }
-            if brightness > 180 {
-                return "#";
-            }
-            if brightness > 145 {
-                return "e";
-            }
-            if brightness > 110 {
-                return "o";
-            }
-            if brightness > 75 {
-                return ",";
-            }
-            if brightness > 40 {
-                return ".";
-            }
-            return " ";
-        }
-
-        if self == &CharSet::Discord {
-
-            // :white_large_square: :fog: :elephant: :new_moon:  :black_large_square: 
-
-            if brightness > 200 {
-                return ":white_large_square:";
-            }
-            if brightness > 150 {
-                return ":fog:";
-            }
-            if brightness > 100 {
-                return ":elephant:";
-            }
-            if brightness > 50 {
-                return ":new_moon:";
-            }
-            return ":black_large_square:";
-        }
-
-        if self == &CharSet::Braile {
-
-            // ⣿⣫⢕⡈⠀   <- dont forget the invisible character at the end
-
-            if brightness > 200 {
-                return "⣿";
-            }
-            if brightness > 150 {
-                return "⣫";
-            }
-            if brightness > 100 {
-                return "⢕";
-            }
-            if brightness > 50 {
-                return "⡈";
-            }
-            // this is not a space, this is an empty braile character
-            return "⠀";
-        }
-
-        return " ";
+        return set[id.round() as usize];
     }
 }
 
-fn get_brightness(pixel: Rgba<u8>) -> u8 {
-    let red = pixel.0[0] as f64;
-    let green = pixel.0[1] as f64;
-    let blue = pixel.0[2] as f64;
-    let alpha = pixel.0[3] as f64;
+fn get_cols_and_rows(
+    character_dimensions: Option<Vec<u32>>,
+    columns: Option<u32>,
+    rows: Option<u32>,
+    image_width: u32,
+    image_height: u32,
+) -> (u32, u32) {
+    let char_width: u32;
+    let char_height: u32;
+    if let Some(vec) = character_dimensions {
+        char_width = vec.get(0).unwrap().clone();
+        char_height = vec.get(1).unwrap().clone();
+    } else {
+        // TODO default to measured from terminal, if unavailable default to sensible numbers
+        char_width = 10;
+        char_height = 20;
+    }
+
+    let (columns, rows) = match (columns, rows) {
+        (Some(x), Some(y)) => (x, y), // take user inputted cols and rows, resolution might be distorted tho
+        (Some(x), None) => (
+            x,
+            calculate_other_dimension(x, char_height, char_width, image_height, image_width),
+        ),
+        (None, Some(y)) => (
+            calculate_other_dimension(y, char_width, char_height, image_width, image_height),
+            y,
+        ),
+        (None, None) => get_fitting_terminal(char_width, char_height, image_width, image_height),
+    };
+
+    println!("columns: {}, rows: {}", columns, rows);
+
+    (columns, rows)
+}
+
+// TODO stupid func name, stupid var names
+fn calculate_other_dimension(
+    char1: u32,
+    thicness1: u32,
+    thicness2: u32,
+    image1: u32,
+    image2: u32,
+) -> u32 {
+    (char1 * (image2 / thicness2)) / (image1 / thicness1)
+    // TODO decide how i want this rounded or not
+}
+
+fn get_fitting_terminal(
+    char_width: u32,
+    char_height: u32,
+    image_width: u32,
+    image_height: u32,
+) -> (u32, u32) {
+    // TODO get terminal max x and y
+    // TODO this also might not be the best place to get those, maybe in the calling method
+    let max_terminal_chars_x = 213;
+    let max_terminal_chars_y = 55;
+
+    let y_chars = calculate_other_dimension(
+        max_terminal_chars_x,
+        char_width,
+        char_height,
+        image_width,
+        image_height,
+    );
+
+    if y_chars <= max_terminal_chars_y {
+        return (y_chars, max_terminal_chars_x);
+    }
+
+    let x_chars = calculate_other_dimension(
+        max_terminal_chars_y,
+        char_height,
+        char_width,
+        image_height,
+        image_width,
+    );
+    return (max_terminal_chars_y, x_chars);
+}
+
+// returns brightness between 0 and 1
+fn get_brightness(pixel: Rgba<u8>) -> f32 {
+    let red = pixel.0[0] as f32;
+    let green = pixel.0[1] as f32;
+    let blue = pixel.0[2] as f32;
+    let alpha = pixel.0[3] as f32;
 
     // source https://en.wikipedia.org/wiki/Relative_luminance
-    return ((red * 0.2126) + (green * 0.7152) + (blue * 0.0722) * (alpha / 255.0)) as u8;
+    return ((red * 0.2126) + (green * 0.7152) + (blue * 0.0722) * (alpha / 255.0)) / 255.0;
 }
 
 fn main() {
     let args = Args::parse();
 
-    // TODO is there a neater way to do this?
     let reader_result = Reader::open(args.path);
     if reader_result.is_err() {
-        println!("\x1b[33mCant find image");
+        println!("\x1b[33mCannot find image");
         return;
     }
     let img_result = reader_result.unwrap().decode();
     if img_result.is_err() {
-        println!("\x1b[33mCant open image");
+        println!("\x1b[33mCannot open image");
         return;
     }
     let image = img_result.unwrap();
 
+    println!("width: {}, height: {}", image.width(), image.height());
 
-    // match aspect ratio
-    let img_aspect_ratio = image.width() as f32 / image.height() as f32;
-    // todo this will differ between terminal and charset, might need to fix this but difference might be ignorable
-    let char_aspect_ratio = 1 as f32; 
+    let (columns, rows) = get_cols_and_rows(
+        args.character_size,
+        args.width,
+        args.height,
+        image.width(),
+        image.height(),
+    );
 
-    let matched_height = (args.width as f32 / char_aspect_ratio / img_aspect_ratio).round() as u32;
+    let pixel_info = preprocess_image(image, args.lines);
 
-    println!("{}", process_image(image, args.width, matched_height, args.set, args.color, args.inverted));
+    let result = translate_to_text(
+        pixel_info,
+        columns,
+        rows,
+        args.set,
+        args.color,
+        args.inverted,
+    );
+
+    println!("{}", result);
 }
 
-fn process_image(image: DynamicImage, width: u32, height: u32, set: CharSet, color: ColorSet, inverted: bool) -> String {
+struct PixelData {
+    edgeness: f32,
+    direction: f32,
+    brightness: f32,
+    // TODO color???
+}
 
+// this will no doubt use a lot of memmory, no clue what to do about it tho :p
+fn preprocess_image(image: DynamicImage, lines: bool) -> Vec<Vec<PixelData>> {
+    let mut result: Vec<Vec<PixelData>> = vec![];
+    for y in 0..image.height() {
+        result.insert(y as usize, vec![]);
+        for x in 0..image.width() {
+            // process
+            let brightness = get_brightness(image.get_pixel(x, y));
+
+            // store
+            result.get_mut(y as usize).unwrap().insert(
+                x as usize,
+                PixelData {
+                    edgeness: 0.0,
+                    direction: 0.0,
+                    brightness: brightness,
+                },
+            );
+        }
+    }
+    result
+}
+
+fn translate_to_text(
+    image: Vec<Vec<PixelData>>,
+    columns: u32,
+    rows: u32,
+    set: CharSet,
+    color: ColorSet,
+    inverted: bool,
+) -> String {
     let mut result = "".to_string();
 
     // iterate over parts of image
-    for y in 0..height {
-        let pixel_y_min = (image.height() as f32 * y as f32 / height as f32) as u32;
-        let pixel_y_max = (image.height() as f32 * (y + 1) as f32 / height as f32) as u32 - 1;
+    for y in 0..columns {
+        let pixel_y_min = (image.len() as f32 * y as f32 / columns as f32) as u32;
+        let pixel_y_max = (image.len() as f32 * (y + 1) as f32 / columns as f32) as u32;
 
-        for x in 0..width {
-            let pixel_x_min = (image.width() as f32 * x as f32 / width as f32) as u32;
-            let pixel_x_max = (image.width() as f32 * (x + 1) as f32 / width as f32) as u32 - 1;
-            
+        for x in 0..rows {
+            let pixel_x_min =
+                (image.get(0).unwrap().len() as f32 * (x as f32 / rows as f32)) as u32;
+            let pixel_x_max =
+                (image.get(0).unwrap().len() as f32 * ((x + 1) as f32 / rows as f32)) as u32;
+
             // get average value of part
-            let mut pixel = get_center_pixel(&image, pixel_x_min, pixel_x_max, pixel_y_min, pixel_y_max);
+            let pixel =
+                get_center_pixel(&image, pixel_x_min, pixel_x_max, pixel_y_min, pixel_y_max);
 
             if inverted {
-                pixel.invert();
+                // TODO invert brightness value
             }
 
             // place char in result string
-            result += color.get_color_prefix(pixel);
-            result += set.get_char(pixel);
+
+            // TODO color here
+            // result += color.get_color_prefix(pixel);
+
+            result += set.get_char(pixel.brightness);
         }
         result += "\n";
     }
@@ -232,28 +327,18 @@ fn process_image(image: DynamicImage, width: u32, height: u32, set: CharSet, col
     return result;
 }
 
-fn get_center_pixel(image: &DynamicImage, x_min: u32, x_max: u32, y_min: u32, y_max: u32) -> Rgba<u8> {
-    image.get_pixel((x_min + x_max) / 2, (y_min + y_max) / 2)
-}
-
-// TODO, does this actually provide something?
-fn get_blended_pixel(image: &DynamicImage, x_min: u32, x_max: u32, y_min: u32, y_max: u32) -> Rgba<u8> {
-    let width = x_max - x_min;
-    let height = y_max - y_min;
-
-    let x = x_min;
-    let y = y_min;
-
-    // TODO clean up, for now it takes the average of 4 pixels spread around the given area
-    let mut pixel1 = image.get_pixel(x + (    width / 4),  y + (    height / 4));
-    let mut pixel2 = image.get_pixel(x + (    width / 4),  y + (3 * height / 4));
-    let mut pixel3 = image.get_pixel(x + (3 * width / 4),  y + (    height / 4));
-    let mut pixel4 = image.get_pixel(x + (3 * width / 4),  y + (3 * height / 4));
-
-    pixel1.blend(&pixel2);
-    pixel3.blend(&pixel4);
-
-    pixel1.blend(&pixel3);
-
-    return pixel1;
+fn get_center_pixel(
+    image: &Vec<Vec<PixelData>>,
+    x_min: u32,
+    x_max: u32,
+    y_min: u32,
+    y_max: u32,
+) -> &PixelData {
+    // println!("min x: {} y: {}", x_min, y_min);
+    // println!("max x: {} y: {}", x_max, y_max);
+    image
+        .get(((y_min + y_max) / 2) as usize)
+        .unwrap()
+        .get(((x_min + x_max) / 2) as usize)
+        .unwrap()
 }
