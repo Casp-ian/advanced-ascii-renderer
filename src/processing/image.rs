34db2a6126eaeef1fc350efdbef0f50bcccc2e42
caspian <caspian@koolwaaij.nl>
2024-12-13 21:59:04 +0100

    half of edge detection

--- src/processing/image.rs
+++ src/processing/image.rs
@@ -1,3 +1,4 @@
+use std::f32::consts::PI;
 use std::io::Cursor;
 
 use image::{DynamicImage, Rgb, Rgba};
@@ -36,7 +37,11 @@ impl Magic {
     }
 
     fn setup_gpu(&mut self) {
-        self.gpu = Some(gpu::WgpuContext::setup(64, 40, 64, 40).block_on().unwrap());
+        self.gpu = Some(
+            gpu::WgpuContext::setup(256, 160, 256, 160)
+                .block_on()
+                .unwrap(),
+        );
     }
 
     pub fn do_magic(&mut self, image: DynamicImage) -> String {
@@ -45,8 +50,8 @@ impl Magic {
             self.args.char_height,
             self.args.height,
             self.args.width,
-            64, // TODO configurable
-            40,
+            256, // TODO configurable
+            160,
         );
 
         // TODO handle failure
@@ -56,7 +61,7 @@ impl Magic {
         let gpu = self.gpu.as_ref().unwrap();
 
         let color_buffer = image
-            .resize_to_fill(64, 40, image::imageops::FilterType::Nearest)
+            .resize_to_fill(256, 160, image::imageops::FilterType::Nearest)
             .to_rgba8();
         let buffer = gpu.process(color_buffer.clone()).block_on().unwrap();
 
@@ -67,14 +72,15 @@ impl Magic {
             .chunks_exact(4)
             .zip(color_buffer.chunks_exact(4))
             .map(|x| PixelData {
-                direction: Direction::None,
+                direction: get_direction(x.0[0], x.0[1]),
+                // direction: Direction::None,
                 brightness: get_brightness(x.1),
                 color: Rgb(x.1[0..3].try_into().expect("color buffer is broken")),
             })
             .collect();
 
         let result_string = translate_to_text(
-            data.chunks(64)
+            data.chunks(256)
                 .map(|x| x.to_vec())
                 .collect::<Vec<Vec<PixelData>>>(),
             columns,
@@ -89,6 +95,40 @@ impl Magic {
     }
 }
 
+fn get_direction(gx: u8, gy: u8) -> Direction {
+    let gx = (gx as f32 / 256.0);
+    let gy = (gy as f32 / 256.0);
+    let magnitude_threshold = 0.8;
+    let magnitude = (gx.powi(2) + gy.powi(2)).sqrt();
+    let dir = gy.atan2(gx);
+
+    let direction: Direction;
+    if magnitude > magnitude_threshold {
+        if (dir < (2.0 * PI / 3.0) && dir > (PI / 3.0))
+            || (dir < (-2.0 * PI / 3.0) && dir > (-1.0 * PI / 3.0))
+        {
+            direction = Direction::LeftToRight;
+        } else if ((dir < PI / 6.0) && (dir > -1.0 * PI / 6.0))
+            || ((dir > 5.0 * PI / 6.0) || (dir < -5.0 * PI / 6.0))
+        {
+            direction = Direction::TopToBottom;
+        } else if ((dir > PI / 6.0) && (dir < PI / 3.0))
+            || ((dir > -5.0 * PI / 6.0) && (dir < -2.0 * PI / 3.0))
+        {
+            direction = Direction::ToprightToBotleft;
+        } else if ((dir < -1.0 * PI / 6.0) && (dir > -1.0 * PI / 3.0))
+            || ((dir < 5.0 * PI / 6.0) && (dir > 2.0 * PI / 3.0))
+        {
+            direction = Direction::TopleftToBotright;
+        } else {
+            direction = Direction::None;
+        }
+    } else {
+        direction = Direction::None;
+    }
+
+    return direction;
+}
 // returns brightness between 0 and 1
 fn get_brightness(colors: &[u8]) -> f32 {
     let red = colors[0] as f32;
--- src/processing/shader.wgsl
+++ src/processing/shader.wgsl
@@ -6,19 +6,31 @@ var inputTexture: texture_2d<f32>;
 @binding(1)
 var outputTexture: texture_storage_2d<rgba8unorm, write>; // this is used as both input and output for convenience
 
+fn average(vec: vec3<f32>) -> f32 {
+    return dot(vec, vec3<f32>(1.0 / 3.0)); // Efficient averaging using the dot product
+}
+
 @compute
 @workgroup_size(1)
 fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
 
-    let color = abs(
-          1 * textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y - 1), 0).rgb
-        + 2 * textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y + 0), 0).rgb
-        + 1 * textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y + 1), 0).rgb
-        - 1 * textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y - 1), 0).rgb
-        - 2 * textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y + 0), 0).rgb
-        - 1 * textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y + 1), 0).rgb
+    let gx = abs(
+          1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y - 1), 0).rgb)
+        + 2 * average(textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y + 0), 0).rgb)
+        + 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y + 1), 0).rgb)
+        - 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y - 1), 0).rgb)
+        - 2 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y + 0), 0).rgb)
+        - 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y + 1), 0).rgb)
     );
-    textureStore(outputTexture, global_id.xy, vec4<f32>(color, 1.0));    // let texel = textureLoad(inputTexture, vec2<u32>(global_id.x, global_id.y), 0);
+    let gy = abs(
+          1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y - 1), 0).rgb)
+        + 2 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 0, global_id.y - 1), 0).rgb)
+        + 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y - 1), 0).rgb)
+        - 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x - 1, global_id.y + 1), 0).rgb)
+        - 2 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 0, global_id.y + 1), 0).rgb)
+        - 1 * average(textureLoad(inputTexture, vec2<u32>(global_id.x + 1, global_id.y + 1), 0).rgb)
+    );
+    textureStore(outputTexture, global_id.xy, vec4<f32>(gx, gy, 0.0, 0.0));    // let texel = textureLoad(inputTexture, vec2<u32>(global_id.x, global_id.y), 0);
     // textureStore(outputTexture, vec2<u32>(global_id.x, global_id.y), texel);
     // Determine the pixel position in the texture
 }
